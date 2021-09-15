66f0649235ab0964987aeac9a62952703ca12b3c
liuxiaoyu-LXY <77136287+liuxiaoyu-LXY@users.noreply.github.com>
2021-09-15 14:28:30 -0400

    Delete 轮播组件封装 directory

--- 轮播组件封装/src/js/constant.js
+++
@@ -1,3 +0,0 @@
-export const ELEMENT_NODE_TYPE = 1;
-export const AnimationClass='slider-transition';
-export const default_className='slider-item';
--- 轮播组件封装/src/js/default.js
+++
@@ -1,9 +0,0 @@
-const DEFAULTS = {
-    startIndex:0,
-    
-    LASTTIME:0.8,
-    ActiveName:'slider-item-active',
-   Animation:0,
-   autoplay:1
-}
-export default DEFAULTS
